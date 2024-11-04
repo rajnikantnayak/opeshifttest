10d8e1e6b4112efd9960bdfd8eb3ae9672b0fb6f
rajnikant.nayak <rajnikantnayak100@gmail.com>
2024-11-04 10:28:45 +0530

    first commit

--- src/main/java/com/example/openshift/demo/MessageController.java
+++ src/main/java/com/example/openshift/demo/MessageController.java
@@ -3,7 +3,6 @@ package com.example.openshift.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
